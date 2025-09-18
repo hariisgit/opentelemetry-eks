54c706f16e17d8fabf868e49043e5391829dbf5a
Shrihari Pawnikar <hari@Shriharis-MacBook-Air.local>
2025-09-18 18:26:56 +0530

    NAT GW correction for private route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -74,7 +74,7 @@ resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.eks_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.eks_nat_gw[count.index].id
+    nat_gateway_id = aws_nat_gateway.eks_nat_gw[count.index].id
   }
 }
 
